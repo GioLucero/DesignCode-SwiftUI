d5ea68f03d8aefa6d0731713b3509a9bd2c5c24d
GioLucero <gioandrewlucero@gmail.com>
2019-10-22 11:13:20 -0400

    Added effects to components.

--- SwiftUI Course/ContentView.swift
+++ SwiftUI Course/ContentView.swift
@@ -13,20 +13,28 @@ struct ContentView: View {
         // ZStack allows you to stack items on top of each other
         ZStack {
             
+            TitleView()
+                .blur(radius: 20)
+            
+            CardBottomView()
+                .blur(radius: 20)
+            
             CardView()
                 .offset(x: 0, y: -40)
                 .scaleEffect(0.85)
-                .rotation3DEffect(Angle(degrees: 50.0), axis (x: 10.0, y: 10.0, z:10.0))
+                .rotation3DEffect(Angle(degrees: 50.0), axis: (x: 10.0, y: 10.0, z:10.0))
+                .blendMode(.hardLight)
             
             CardView()
                 .offset(x: 0, y: -20)
                 .scaleEffect(0.9)
-                .rotation3DEffect(Angle(degrees: 40), axis (x: 10.0, y: 10.0, z:10.0))
+                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10.0, y: 10.0, z:10.0))
+                .blendMode(.hardLight)
             
             
             CertificateView()
                 .scaleEffect(0.95)
-                .rotation3DEffect(Angle(degrees: 30), axis (x: 10.0, y: 10.0, z:10.0))
+                .rotation3DEffect(Angle(degrees: 30), axis: (x: 10.0, y: 10.0, z:10.0))
         }
     }
     
@@ -76,5 +84,43 @@ struct CertificateView: View {
         .background(Color.black)
         .cornerRadius(10)
         .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3.0)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .lineLimit(10)
+            Spacer()
+        }
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600)
     }
 }
