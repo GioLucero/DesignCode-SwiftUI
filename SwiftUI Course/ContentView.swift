f984017a240fc389f356f8d1464dbc463212dd33
GioLucero <gioandrewlucero@gmail.com>
2019-10-22 12:37:39 -0400

    Added animations and blurs to card views.

--- SwiftUI Course/ContentView.swift
+++ SwiftUI Course/ContentView.swift
@@ -9,32 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    
     var body: some View {
+        
         // ZStack allows you to stack items on top of each other
         ZStack {
             
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             CardBottomView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             CardView()
+                .background(Color.blue)
                 .offset(x: 0, y: -40)
                 .scaleEffect(0.85)
-                .rotation3DEffect(Angle(degrees: 50.0), axis: (x: 10.0, y: 10.0, z:10.0))
+                .rotationEffect(Angle(degrees: show ? 15 : 0))
+//                .rotation3DEffect(Angle(degrees: 50.0), axis: (x: 10.0, y: 10.0, z:10.0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.7))
+                .offset(x: 0, y: show ? -400 : -40)
             
             CardView()
+                .background(show ? Color.red : Color.blue)
                 .offset(x: 0, y: -20)
                 .scaleEffect(0.9)
-                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10.0, y: 10.0, z:10.0))
+                .rotationEffect(Angle(degrees: show ? 10 : 0))
+//                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10.0, y: 10.0, z:10.0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+                .offset(x: 0, y: show ? -200 : -20)
             
             
             CertificateView()
                 .scaleEffect(0.95)
-                .rotation3DEffect(Angle(degrees: 30), axis: (x: 10.0, y: 10.0, z:10.0))
+                .rotationEffect(Angle(degrees: show ? 5 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 30 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .animation(.spring())
+                .onTapGesture {
+                    self.show.toggle()
+                }
         }
     }
     
